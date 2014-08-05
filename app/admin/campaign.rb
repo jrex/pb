bd38d459c9dcec41e94791495b34f0f780dd0a8d
Prelang Builder <builder@prelang.com>
2014-08-05 04:40:31 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Campaign' to allow for create, update

--- app/admin/campaign.rb
+++ app/admin/campaign.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Campaign do
+  permit_params :title, :recipient, :redirect_url, :description, :bitcoin, :dogecoin, :litecoin, :btc_address, :dogecoin_address, :litecoin_address, :active, :btc_total_in_satoshis, :ltc_total_in_satoshis, :doge_total_in_satoshis, :archived
 
 
   # See permitted parameters documentation:

12e6994d06d2aac1d85e098fce2b2fe14e0738c0
Prelang Builder <builder@prelang.com>
2014-08-05 04:40:20 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Tip' to allow for create, update

--- app/admin/tip.rb
+++ app/admin/tip.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Tip do
+  permit_params :received_at, :tx_id, :name, :email, :btc_amount_in_satoshis, :ltc_amount_in_satoshis, :doge_amount_in_satoshis, :sender_address, :usd_value_in_cents, :btc_fee_in_satoshis, :ltc_fee_in_satoshis, :doge_fee_in_satoshis, :recipient_address, :fee_in_usd
 
 
   # See permitted parameters documentation:
